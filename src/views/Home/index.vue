6626bb33e7ba84e7ff969f21380aeead6429123f
“Yi” <2637034630@qq.com>
2022-07-27 23:35:45 +0800

    初始化

---
+++ src/views/Home/index.vue
@@ -0,0 +1,18 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div id="home">
+    <div>Home</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style lang="scss" scoped>
+div{
+  color: pink;
+}
+</style>
