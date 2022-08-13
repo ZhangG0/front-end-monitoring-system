13245b3cc63cf8c3fe37aa7c4aea226bed27119d
ddc-hust <1966423145@qq.com>
2022-08-13 21:21:49 +0800

    .

--- src/main.js
+++ src/main.js
@@ -4,7 +4,10 @@ import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import  * as  echarts from 'echarts'
-
+import "@/style/index.scss"
+import * as dayjs from 'dayjs'
+import * as isLeapYear from 'dayjs/plugin/isLeapYear' // import plugin
+import 'dayjs/locale/zh-cn' // import locale
 
 dayjs.extend(isLeapYear) // use plugin
 dayjs.locale('zh-cn') // use locale
